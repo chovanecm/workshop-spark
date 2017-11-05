482b94c990c246515b943022d43b1e00cb354603
Ondrej Kucera <ondra.kuca@gmail.com>
2017-11-05 01:12:40 +0100

    Update WordCount.scala

--- scala/task1-wordcount/src/main/scala/org/workshop/WordCount.scala
+++ scala/task1-wordcount/src/main/scala/org/workshop/WordCount.scala
@@ -9,15 +9,15 @@ object WordCount {
       .appName("World Count")
       .getOrCreate()
 
-    val lines = spark.read.textFile(arg(0))
-    val words = lines.flatMap(line => line.split(" ").filter(w => w != null && !w.isEmpty))
-    val pairs = words.map(word => (word, 1))
-    val freqs = pairs.reduceByKey((a, b) => a + b)
-    val top = freqs.sortBy(_._2, false)
+//     val lines = spark.read.textFile(arg(0))
+//     val words = lines.flatMap(line => line.split(" ").filter(w => w != null && !w.isEmpty))
+//     val pairs = words.map(word => (word, 1))
+//     val freqs = pairs.reduceByKey((a, b) => a + b)
+//     val top = freqs.sortBy(_._2, false)
 
-    val top10 = top.take(10)
+//     val top10 = top.take(10)
 
-    top10.foreach(println)
+//     top10.foreach(println)
 
     spark.stop()
   }
